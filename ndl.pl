6e9a7b1662a02cc982dc31915328d22e68c42d8d
Michael Hendricks <michael@ndrix.org>
2015-01-10 19:37:11 -0700

    Better parsing of the command line
    If the user provides two search patterns, notify the user that he did
    something wrong.

--- ndl.pl
+++ ndl.pl
@@ -2,9 +2,9 @@
 
 :- dynamic pattern/1.
 
-main([Pattern]) :-
+main(Args) :-
     get_time(Start),
-    asserta(pattern(Pattern)),
+    ( assert_environment(Args) -> true; halt(1) ),
 
     phrase(search,Pipeline),
     warn("~s~n", [Pipeline]),
@@ -18,6 +18,21 @@ main([Pattern]) :-
 runtime(Start,Stop) :-
     Duration is 1000*(Stop - Start),
     format("Runtime: ~1f ms~n", [Duration]).
+
+
+assert_environment(Args) :-
+    maplist(assert_arg,Args).
+
+assert_arg(Arg) :-
+    assert_arg_pattern(Arg).
+
+assert_arg_pattern(Second) :-
+    pattern(First),
+    warn("Two patterns given: '~s' then '~s'~n", [First,Second]),
+    !,
+    fail.
+assert_arg_pattern(Pattern) :-
+    assertz(pattern(Pattern)).
 
 
 warn(Format,Args) :-
