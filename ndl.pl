21c5d368bab9aca80975459bb0d1232ced2be63c
Michael Hendricks <michael@ndrix.org>
2015-01-10 18:33:03 -0700

    Output Prolog runtime duration
    This doesn't cover the time required by the OS to load and clean up
    after the executable, but it gives a good idea how much time is spent
    creating and executing the search pipeline.

--- ndl.pl
+++ ndl.pl
@@ -3,10 +3,18 @@
 :- dynamic pattern/1.
 
 main([Pattern]) :-
+    get_time(Start),
     asserta(pattern(Pattern)),
 
     phrase(search,Pipeline),
     format(user_error, "~s~n", [Pipeline]),
 
     shell(Pipeline, Status),
+    get_time(Stop),
+    runtime(Start, Stop),
     halt(Status).
+
+
+runtime(Start,Stop) :-
+    Duration is 1000*(Stop - Start),
+    format("Runtime: ~1f ms~n", [Duration]).
