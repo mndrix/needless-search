bc14de88703a3b845acd56a36750756d23c39d69
Michael Hendricks <michael@ndrix.org>
2015-01-16 16:26:31 -0700

    Adjust for files that mdfind doesn't index
    mdfind doesn't index .json or .md files.  We can only use mdfind if
    we're certain that those files don't contain any matches.  Typically
    this means that the user has asked to exclude those files by restricting
    searches to files of another language.

--- src/mdfind.pl
+++ src/mdfind.pl
@@ -2,6 +2,8 @@
 
 mdfind -->
     { pattern(Pattern) },
+    { \+ mdfind_overlooks_our_files },
+
     "mdfind",
     " -onlyin .",
     " -0 ",
@@ -13,8 +15,26 @@ query(Query) -->
     query_(Query),
     "\"".
 
+query_((A;B)) -->
+    query_(A),
+    " || ",
+    query_(B).
 query_(Key==Value) -->
     "kMDItem",
     atom(Key),
     "==",
     "'", atom(Value), "'".
+
+% true if mdfind doesn't index the files we want to search
+mdfind_overlooks_our_files :-
+    % without a whitelisted language, mdfind will miss something
+    \+ include_lang(_).
+mdfind_overlooks_our_files :-
+    include_lang(Lang),
+    lang_extension(Lang,Ext),
+    mdfind_wont_index_extension(Ext).
+
+
+% mdfind doesn't index files with these extensions
+mdfind_wont_index_extension(json).
+mdfind_wont_index_extension(md).
