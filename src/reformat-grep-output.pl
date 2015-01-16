828828e2f491ad8dfa693b8eef1ac5e176c1a765
Michael Hendricks <michael@ndrix.org>
2015-01-16 13:29:29 -0700

    Much prettier output formatting
    Aside from using relative paths for the filenames, this matches ack and
    ag output.

--- src/reformat-grep-output.pl
+++ src/reformat-grep-output.pl
@@ -5,19 +5,16 @@
 % predicate has IO and logic intertwined, not as clean as I'd like
 reformat_grep_output(Pattern) :-
     prompt(_,''),
-    reformat_grep_output_(Pattern).
+    reformat_grep_output_(no(previous),Pattern).
 
-reformat_grep_output_(Pattern) :-
+reformat_grep_output_(PrevPath,Pattern) :-
     read_line_to_codes(current_input,Line),
     Line \= end_of_file,
     !,
     phrase(grep_line(Path,N,Content),Line),
-    ansi_format([bold,fg(green)],"~s",[Path]),
-    nl,
-    ansi_format([bold,fg(yellow)],"~d", [N]),
-    format(":~s~n",[Content]),
-    reformat_grep_output_(Pattern).
-reformat_grep_output_(_).
+    print_line(PrevPath,Path,N,Content),
+    reformat_grep_output_(Path,Pattern).
+reformat_grep_output_(_,_).
 
 
 grep_line(Path,N,Content) -->
@@ -26,3 +23,15 @@ grep_line(Path,N,Content) -->
     integer(N),
     ":",
     rest_of_line(Content).
+
+
+print_line(PrevPath,Path,N,Content) :-
+    ( PrevPath = Path ->
+        true  % no header line needed
+    ; otherwise ->
+        ( PrevPath = no(previous) -> true; nl ),
+        ansi_format([bold,fg(green)],"~s",[Path]),
+        nl
+    ),
+    ansi_format([bold,fg(yellow)],"~d", [N]),
+    format(":~s~n",[Content]).
