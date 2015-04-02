2b36deb2a71c1ec7e9e2ab55d0cb81325eff55cc
Michael Hendricks <michael@ndrix.org>
2015-04-02 17:02:43 -0600

    Use 'git check-ignore' to handle Git ignore files
    This command parses the ignore files once and avoids traversing the
    directory tree.  Because 'git check-ignore' doesn't have a filter mode,
    the implementation is kind of ugly.

--- src/search.pl
+++ src/search.pl
@@ -29,9 +29,35 @@ search -->
 respect_gitignore -->
     { inside_git_repository },
     !,
-    " | ",
-    { executable(Exe) },
-    atom(Exe),
-    " --git-ignore-files ".
+    " | git check-ignore -z --stdin --non-matching --verbose",
+
+    % See Note_1
+    " | perl -0lne 'if($. % 4==3 && !$_) { $_=<>; print }'".
 respect_gitignore -->
     [].
+
+
+/* Note_1
+
+'git check-ignore' has no way to read stdin and filter out files that
+should be ignored.  It seems like 'git check-ignore --stdin -n' should do
+the job, but that command refuses to run without the --verbose option.
+
+This Perl one-liner is a nasty workaround to strip out the verbose output
+and select only those files which should not be ignored.
+
+The verbose output is like this:
+
+ <source> <NULL> <linenum> <NULL> <pattern> <NULL> <pathname> <NULL>
+
+So there are 4 columns per record.  If the 3rd column is empty, there is
+no gitignore pattern matching the file in question.  In that case, read
+the next column and print it.
+
+Justification for each Perl option:
+
+  -0 : use null terminator
+  -l : auto chomp and set output separator to match input separator
+  -n : process each "line" (a null-separated chunk)
+  -e : next argument is the script
+*/
